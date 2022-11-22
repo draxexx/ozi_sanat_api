5941b35db3ba52b1f2067bdfb26762b50b863165
burak <muhamm3d_@hotmail.com>
2022-11-22 23:41:59 +0300

    add timezone to config.js to handle timezone issue

--- config/config.js
+++ config/config.js
@@ -13,13 +13,37 @@ module.exports = {
     password: process.env.DB_LOCAL_PASSWORD,
     database: process.env.DB_LOCAL_DATABASE,
     host: process.env.DB_LOCAL_HOST,
+    dialectOptions: {
+      useUTC: false, //for reading from database
+      dateStrings: true,
+      typeCast: function (field, next) {
+        // for reading from database
+        if (field.type === "DATETIME") {
+          return field.string();
+        }
+        return next();
+      },
+    },
     dialect: "mysql",
+    timezone: "+03:00",
   },
   production: {
     username: process.env.DB_PROD_USER,
     password: process.env.DB_PROD_PASSWORD,
     database: process.env.DB_PROD_DATABASE,
     host: process.env.DB_PROD_HOST,
+    dialectOptions: {
+      useUTC: false, //for reading from database
+      dateStrings: true,
+      typeCast: function (field, next) {
+        // for reading from database
+        if (field.type === "DATETIME") {
+          return field.string();
+        }
+        return next();
+      },
+    },
     dialect: "mysql",
+    timezone: "+03:00",
   },
 };
