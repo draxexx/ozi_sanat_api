7c49c22b46b99a08dc572c58a342262138ca782e
burak <muhamm3d_@hotmail.com>
2023-02-04 15:12:48 +0300

    get branch non paying student list

--- controllers/branch_manage.js
+++ controllers/branch_manage.js
@@ -1,4 +1,10 @@
-const { branch_manage, branch, branch_course, user } = require("../models");
+const {
+  branch_manage,
+  branch,
+  branch_course,
+  user,
+  student_payment,
+} = require("../models");
 const { findUniqueItem } = require("../helpers/find_unique_in_list");
 
 const getAllBranchManages = async (req, res, next) => {
@@ -81,6 +87,70 @@ const getBranchStudents = async (req, res, next) => {
   }
 };
 
+const getBranchNonPayingStudents = async (req, res, next) => {
+  try {
+    const singleBranchManage = await branch_manage.findOne({
+      where: {
+        teacherId: req.params.id,
+        active: true,
+      },
+      //   attributes: [],
+      order: [
+        ["branch", "branchCourses", "students", "firstName", "ASC"],
+        ["branch", "branchCourses", "students", "lastName", "ASC"],
+      ],
+      include: {
+        model: branch,
+        as: "branch",
+        include: {
+          model: branch_course,
+          as: "branchCourses",
+          attributes: ["id"],
+          include: {
+            model: user,
+            as: "students",
+            where: {
+              active: true,
+            },
+            include: {
+              model: student_payment,
+              as: "payments",
+              where: {
+                isPaymentCompleted: false,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    let tempStudents = [];
+
+    singleBranchManage.branch.branchCourses.forEach((element) => {
+      element.students.forEach((student) => {
+        tempStudents.push(student);
+      });
+    });
+
+    const data = findUniqueItem(tempStudents);
+
+    return res.json({
+      code: res.statusCode,
+      status: "success",
+      message: "Şube öğrencileri başarıyla getirildi.",
+      data: data,
+    });
+  } catch (error) {
+    next(error);
+    return res.status(404).json({
+      code: res.statusCode,
+      status: "error",
+      message:
+        "Şube öğrencileri getirilirken hata meydana geldi, lütfen daha sonra tekrar deneyiniz.",
+    });
+  }
+};
+
 const getBranchByTeacher = async (req, res, next) => {
   try {
     const branchManages = await branch_manage.findAll({
@@ -119,4 +189,5 @@ module.exports = {
   getAllBranchManages,
   getBranchStudents,
   getBranchByTeacher,
+  getBranchNonPayingStudents,
 };
--- routes/branch_manage.js
+++ routes/branch_manage.js
@@ -5,10 +5,12 @@ const {
   getAllBranchManages,
   getBranchStudents,
   getBranchByTeacher,
+  getBranchNonPayingStudents,
 } = require("../controllers/branch_manage");
 
 router.get("/", getAllBranchManages);
 router.get("/branches/:teacherId", getBranchByTeacher);
 router.get("/:id/students", getBranchStudents);
+router.get("/:id/students/nonpaying", getBranchNonPayingStudents);
 
 module.exports = router;
